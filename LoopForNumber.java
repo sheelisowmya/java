9aab27d4ac3e9d7531b064fddd5205d059a269b1
sheelisowmya <sheelisowmya@gmail.com>
2022-08-10 06:06:57 -0700

    Basic program in loops

---
+++ LoopForNumber.java
@@ -0,0 +1,9 @@
+public class LoopForNumber {
+    public static void main(String[] args) {
+        int i, n = 5;
+        for (i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
