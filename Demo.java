86365b53d313d57eac0bacd81dca033243d687c4
sheelisowmya <sheelisowmya@gmail.com>
2022-08-22 10:54:32 -0700

    declared variables

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        int roll=5;
+        float marks=22.5f;
+        System.out.print("my marks are"+marks);
+        System.out.println("and my roll num is"+roll);
+
+    }
+
+}
