da2187624b81b76d95b49a11aa8ec8607ebf36de
sheelisowmya <sheelisowmya@gmail.com>
2022-08-25 07:13:06 -0700

    simple interest

---
+++ SimpleInterest.java
@@ -0,0 +1,10 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int p=10;
+        int t=2;
+        int r=3;
+        int si;
+        si=p*t*r/100;
+        System.out.println(si);
+    }
+}
